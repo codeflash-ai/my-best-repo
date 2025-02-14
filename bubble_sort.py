2f1a879b3c54a30a363b08183e1c0ed2a2691a13
davidgirdwood1 <162387981+davidgirdwood1@users.noreply.github.com>
2025-02-14 15:16:13 -0800

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -6,3 +6,13 @@ def sorter(arr):
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
     return arr
+
+
+def sorter2(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+    return arr
